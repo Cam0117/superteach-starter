8dca6fd75afc5461c5f2ab3263c175c209e44ff9
Cam0117 <A01174422@tec.mx>
2023-06-04 23:45:45 -0600

    mmmaybe

--- pages/utilityfunc.js
+++ pages/utilityfunc.js
@@ -58,15 +58,8 @@ export async function signAndConfirmTransactionFe(network,transaction,callback)
     await phantom.connect();
     const rpcUrl = clusterApiUrl(network);
     const connection = new Connection(rpcUrl,"confirmed");
-    //console.log(connection.rpcEndpoint);
     const ret = await confirmTransactionFromFrontend(connection,transaction,phantom);
-    // const checks = await connection.confirmTransaction({signature:ret},'finalised');
     console.log(ret);
-    // console.log(checks);
-    // await connection.confirmTransaction({
-    //     blockhash: transaction.blockhash,
-    //     signature: ret,
-    //   });
     connection.onSignature(ret,callback,'finalized')
     return ret;
 }
